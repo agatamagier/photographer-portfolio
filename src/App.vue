84f5362c8b9f75f9013e513eb861231d0bc4c23c
Agata Magier <magier.agata@gmail.com>
2018-12-01 20:43:25 +0000

    finish toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,84 @@
 <template>
-  <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
+  <v-app id="app">
+    <v-navigation-drawer v-model="drawer" class="secondary" temporary fixed>
+      <v-list>
+        <v-list-tile v-for="menuItem in menuItems" style="cursor: pointer">
+          <v-list-tile-title :to="menuItem.link"> {{menuItem.title}} </v-list-tile-title>
+        </v-list-tile>
+        <v-list-group>
+          <v-list-tile slot="activator">
+            <v-list-tile-title>Galleries</v-list-tile-title>
+          </v-list-tile>
+          <v-list-tile style="cursor: pointer">
+            <v-list-tile-title to="/weddings">
+              Weddings
+            </v-list-tile-title>
+          </v-list-tile>
+          <v-list-tile style="cursor: pointer">
+            <v-list-tile-title to="/engagements">
+              Engagements
+            </v-list-tile-title>
+          </v-list-tile>
+        </v-list-group>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar   flat class="primary" dark>
+      <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title id="logo"><span>MP</span>hoto<span>V</span>ideo</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn class="toolbar" flat>about</v-btn>
+        <v-btn class="toolbar" flat>offer</v-btn>
+        <v-btn class="toolbar" flat>Gallery</v-btn>
+        <v-btn class="toolbar" flat>testimonials</v-btn>
+        <v-btn class="toolbar" flat>contact</v-btn>
+      </v-toolbar-items>
     </v-toolbar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  },
-  data () {
-    return {
-      //
+  export default {
+    data() {
+      return {
+        drawer: true,
+        menuItems: [{
+            title: 'About',
+            link: '/about'
+          },
+          {
+            title: 'Offer',
+            link: '/offer'
+          },
+          {
+            title: 'Testimonials',
+            link: "/testimonials"
+          },
+          {
+            title: 'Contact',
+            link: '/contact'
+          },
+        ],
+      }
     }
   }
-}
 </script>
+
+<style scoped>
+ @import url('https://fonts.googleapis.com/css?family=Nunito:200,400,700|Sacramento');
+  #app {
+    font-family: 'Nunito', sans-serif;
+  }
+  .toolbar {
+    color: #e8edf3;
+  }
+  #logo {
+    font-family: 'Sacramento', cursive;
+    font-size: 2em;
+    line-height: 2em;
+   
+  }
+  span {
+    color: #e6cf8b;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,22 @@ import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vuetify from 'vuetify'
 
 Vue.config.productionTip = false
+
+Vue.use(Vuetify, {
+  theme: {
+    primary: '#22264b',
+    secondary: '#e8edf3',
+    accent: '#e6cf8b',
+    strong: '#b56969',
+    error: '#f44336',
+    warning: '#ffeb3b',
+    info: '#00ACC1',
+    success: '#43A047'
+  }
+})
 
 new Vue({
   router,
