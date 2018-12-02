9149955108e58a6acd267c34631ec4138700ff89
Agata Magier <magier.agata@gmail.com>
2018-12-02 09:11:51 +0000

    add footer

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,11 @@
         <v-divider></v-divider>
       </v-list>
     </v-navigation-drawer>
-    <v-toolbar   flat class="primary" dark>
+    <v-toolbar flat class="primary" dark>
       <v-toolbar-side-icon class="hidden-sm-and-up" @click="drawer = !drawer"></v-toolbar-side-icon>
-      <v-toolbar-title >
+      <v-toolbar-title>
         <router-link id="logo" to="/"> <span>MP</span>hoto<span>V</span>ideo</router-link>
-       </v-toolbar-title>
+      </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-sm-and-down">
         <v-btn class="toolbar" flat to="/about">about</v-btn>
@@ -22,6 +22,20 @@
         <v-btn class="toolbar" flat to="/contact">contact</v-btn>
       </v-toolbar-items>
     </v-toolbar>
+    <main>
+      <router-view></router-view>
+    </main>
+    <v-footer id="footer" fixed dark height="auto" color="primary" >
+      <v-card flat tile class="footer primary secondary--text text-xs-center">
+        <v-card-text>
+          <v-btn v-for="icon in icons" :key="icon" class="mx-3 secondary--text" icon>
+            <v-icon size="24px">{{ icon }}</v-icon>
+          </v-btn>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-text class="footer secondary--text">Coded with <span>❤ </span>by Agata Magier. &copy; 2018 All rights reserved.</v-card-text>
+      </v-card>
+    </v-footer>
   </v-app>
 </template>
 
@@ -30,6 +44,13 @@
     data() {
       return {
         drawer: null,
+        icons: [
+          'fab fa-facebook',
+          'fab fa-twitter',
+          'fab fa-google-plus',
+          'fab fa-linkedin',
+          'fab fa-instagram'
+        ],
         menuItems: [{
             title: 'About',
             link: '/about'
@@ -57,7 +78,7 @@
 </script>
 
 <style scoped>
- @import url('https://fonts.googleapis.com/css?family=Nunito:200,400,700|Sacramento');
+  @import url('https://fonts.googleapis.com/css?family=Nunito:200,400,700|Sacramento');
   #app {
     font-family: 'Nunito', sans-serif;
   }
@@ -67,14 +88,17 @@
   #logo {
     font-family: 'Sacramento', cursive;
     font-size: 1.7em;
-   cursor: pointer;
-   color: #e8edf3;
-   text-decoration: none;
+    cursor: pointer;
+    color: #e8edf3;
+    text-decoration: none;
   }
   span {
     color: #e6cf8b;
   }
   .drawer_item {
-  font-size: 1.7em;  
+    font-size: 1.7em;
+  }
+  .footer {
+    margin: 0 auto;
   }
 </style>
